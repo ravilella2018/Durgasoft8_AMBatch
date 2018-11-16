c4995af21b295907200f1ecaf3e290e319742354
ravilella2018 <ravilella.2018@gmail.com>
2018-11-16 09:38:10 +0530

    log4j & Dynamic elements

--- Automation/src/main/java/com/project/Automation/BaseTest.java
+++ Automation/src/main/java/com/project/Automation/BaseTest.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -18,62 +20,91 @@ public class BaseTest
 {
 	public static WebDriver driver;
 	public static String path="./config.properties";
+	public static Properties prop;
+	public WebElement e;
 	
-	
-	public static String loadData(String key) throws Exception
-	{
-		Properties p=new Properties();
-		File f=new File(path);
-		FileInputStream fis=new FileInputStream(f);
-		p.load(fis);
-		return p.getProperty(key);
-	}
-	
+
 	public void openBrowser(String browser) throws Exception 
 	{
-		if(browser.equalsIgnoreCase("CHROME"))
+		if(prop==null)
+		{
+			prop=new Properties();
+			File f=new File(path);
+			FileInputStream fis=new FileInputStream(f);
+			prop.load(fis);
+		}
+			
+		
+		if(prop.getProperty(browser).equalsIgnoreCase("CHROME"))
 		{
 			//System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+"//drivers//chromedriver.exe");
-			System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY,loadData("chromedriver_exe") );
+			System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY,prop.getProperty("chromedriver_exe") );
 			driver =new ChromeDriver();
 		}
-		else if (browser.equalsIgnoreCase("FF"))
+		else if (prop.getProperty(browser).equalsIgnoreCase("FF"))
 		{
 			//System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"//drivers//geckodriver.exe" );
-			System.setProperty("webdriver.gecko.driver",loadData("firefoxdriver_exe"));
+			System.setProperty("webdriver.gecko.driver",prop.getProperty("firefoxdriver_exe"));
 			driver=new FirefoxDriver();
 		}
-		else if(browser.equalsIgnoreCase("IE"))
+		else if(prop.getProperty(browser).equalsIgnoreCase("IE"))
 		{
 			//System.setProperty(InternetExplorerDriverService.IE_DRIVER_EXE_PROPERTY,System.getProperty("user.dir")+"//drivers//IEDriverServer.exe" );
-			System.setProperty(InternetExplorerDriverService.IE_DRIVER_EXE_PROPERTY,loadData("iedriver_exe"));
+			System.setProperty(InternetExplorerDriverService.IE_DRIVER_EXE_PROPERTY,prop.getProperty("iedriver_exe"));
 			driver=new InternetExplorerDriver();
 		}
-		else if(browser.equalsIgnoreCase("EDGE"))
+		else if(prop.getProperty(browser).equalsIgnoreCase("EDGE"))
 		{
 			//System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY,System.getProperty("user.dir")+"//drivers//MicrosoftWebDriver.exe" );
-			System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY,loadData("edgedriver_exe"));
+			System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY,prop.getProperty("edgedriver_exe"));
 			driver=new EdgeDriver();
 		}
 		
 		driver.manage().window().maximize();
+		
+		String log4jConfPath="log4j.properties";
+		PropertyConfigurator.configure(log4jConfPath);
 		
 	}
 	
 	
 	public void navigate(String url) 
 	{
-		driver.get(url);
+		driver.get(prop.getProperty(url));
 	}
 	
 	
-	public void text(String eleLocator_id, String value) 
+	public void text(String eleLocatorkey, String value) 
 	{
-		driver.findElement(By.id(eleLocator_id)).sendKeys(value);
+		//driver.findElement(By.id(prop.getProperty(eleLocatorkey))).sendKeys(value);
+		getElement(eleLocatorkey).sendKeys(value);
 	}
 	
-	public void clickElement(String eleLocator_xpath) 
+	
+	public void clickElement(String eleLocatorkey) 
 	{
-		driver.findElement(By.xpath(eleLocator_xpath)).click();
+		//driver.findElement(By.xpath(prop.getProperty(eleLocatorkey))).click();
+		getElement(eleLocatorkey).click();
 	}
+	
+	
+	public WebElement getElement(String element)
+	{
+		
+		
+		if(element.endsWith("_id"))
+		{
+			e=driver.findElement(By.id(prop.getProperty(element)));
+		}
+		else if(element.endsWith("_xpath")) {
+			e=driver.findElement(By.xpath(prop.getProperty(element)));
+		}
+		else if(element.endsWith("_name")) {
+			e=driver.findElement(By.name(prop.getProperty(element)));
+		}
+		
+		return e;
+		
+	}
+	
 }
--- Automation/src/test/java/com/project/Automation/TC_001.java
+++ Automation/src/test/java/com/project/Automation/TC_001.java
@@ -1,17 +1,28 @@
 package com.project.Automation;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 public class TC_001 extends BaseTest
 {
+	public static final Logger log=Logger.getLogger(TC_001.class.getName());
+	
 	
   @Test
-  public void f() throws Exception 
+  public void AmazonSearch() throws Exception 
   {
-	  openBrowser("CHROME");
-	  navigate("https://www.amazon.in");
-	  text("twotabsearchtextbox","sony");
-	  clickElement("//*[@id=\"nav-search\"]/form/div[2]/div/input");
+	  log.info("================ Started Test TC_001 =======================");
+	  
+	  openBrowser("chromebrowser");
+	  log.info("Selected the Browser ---" + prop.getProperty("chromebrowser"));
+	  navigate("amazonurl");
+	  log.info("Navigated to URL --" + prop.getProperty("amazonurl"));
+	  text("amazonsearchtext_id","sony");
+	  log.info("Enter Search keyword By using Locator --" + prop.getProperty("amazonsearchtext_id"));
+	  clickElement("amazonsearchbutton_xpath");
+	  log.info("Clicked on Search Button By using Locator --" + prop.getProperty("amazonsearchbutton_xpath"));
+	  
+	  log.info("================ Ended Test TC_001 =======================");
   }
 
 
